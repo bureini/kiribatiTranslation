2b7472052e93c715b0cc33e3453c597fbd509172
bcode <bureiniuarai@gmail.com>
2026-07-31 10:33:38 +1200

    Upgrade compileSdk version from 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.ekainano.translation"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ekainano.translation"
