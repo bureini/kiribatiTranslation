d73a955ca1fc39a6114772fc008fcccb1ca10d84
bcode <bureiniuarai@gmail.com>
2026-07-31 12:35:02 +1200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.compose.compiler) apply false   // ADD THIS
 }
 
 android {
