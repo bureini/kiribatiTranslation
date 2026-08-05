8f9e3d924ae13580df26c81e2d4f1268fa923aae
bcode <bureiniuarai@gmail.com>
2026-08-06 10:37:01 +1200

    New import added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended")
 
     // Local Room Database (KSP)
     implementation(libs.androidx.room.runtime)
